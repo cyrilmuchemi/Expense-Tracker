94ca245f8f39070b066611a14b3cb5787bd38802
cyrilmuchemi <cyril.muchemi@gmail.com>
2024-04-04 14:18:24 +0300

    Create the UI without functionalities

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import HeaderTemp from './components/HeaderTemp.vue'
+import BalanceTemp from './components/BalanceTemp.vue'
+import incomeExpense from './components/IncomeExpense.vue'
+import TransactionList from './components/TransactionList.vue'
+import AddTransaction from './components/AddTransaction.vue'
+</script>
+
+<template>
+  <HeaderTemp />
+  <div class="container">
+    <BalanceTemp />
+    <incomeExpense />
+    <TransactionList />
+    <AddTransaction />
+  </div>
+</template>
